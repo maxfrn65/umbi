832399404398f2592937c1c1ad255647571057a4
Maxime Fourna <m.fourna@icloud.com>
2025-02-24 10:07:27 +0100

    fix: ci/cd

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import { getDirname } from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
 import adonisjs from '@adonisjs/vite/client'
 
 export default defineConfig({
